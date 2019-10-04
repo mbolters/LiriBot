ff90b446ba5604f895cd3c99ccebc23389325a10
mbolters <mariematisans@gmail.com>
2019-10-04 17:48:44 -0500

    add apis and cases for omdb and bands, have spotify pull by song name

--- liri.js
+++ liri.js
@@ -3,69 +3,74 @@ let keys = require("./keys.js");
 
 let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
-let bandsInTown = new Spotify(keys.spotify);
+
+const axios = require('axios');
 
 let program = process.argv.slice(2)[0];
 
 
 let myArgs = process.argv.slice(3);
-let bandName = "";
+let argString = "";
 
 myArgs.forEach(function(arg){
-    bandName = bandName + arg + '%20';
+    argString = argString + arg + '%20';
 });
 
 switch(program) {
+    
+    
     case 'movie-this':
-        bandsInTown
-        .request('https://api.spotify.com/v1/search?q=' + bandName +'&type=artist')
-        .then(function(artist) {
-          console.log('asdfasdfasfasdfasdfada');   
+        axios.get('http://www.omdbapi.com/?apikey=3089e91d&' + argString)
+        .then(response => {
+            console.log(response);
         })
-        .catch(function(err) {
-          console.error('Error occurred: ' + err); 
+        .catch(error => {
+            console.log(error);
         });
         break;
+
+    
     case 'spotify-this-song' :
         spotify
-        .request('https://api.spotify.com/v1/search?q=' + bandName +'&type=artist')
-        .then(function(artist) {
-          let id = (artist.artists.items[0].id); 
-         
-          spotify
-          .request('https://api.spotify.com/v1/artists/' + id + '/top-tracks?country=US')
-          .then(function(toptracks) {
-              var trackData = [
-                  "Band: " +  toptracks.tracks[0].album.artists[0].name,
-                  "Album: " + toptracks.tracks[0].album.name,
-                  "Top Song: " + toptracks.tracks[0].name,
-                  "Release Date: "+ toptracks.tracks[0].album.release_date,
+        .request('https://api.spotify.com/v1/search?q=' + argString +'&type=track&market=US')
+        .then(function(ids) {
+            console.log(ids.tracks.items[0].id);
+            
+            spotify
+            .request('https://api.spotify.com/v1/artists/' + ids + '/top-tracks?country=US')
+            .then(function(song) {
+                var trackData = [
+                    "Band: " +  song.tracks[0].album.artists[0].name,
+                    "Album: " + song.tracks[0].album.name,
+                    "Top Song: " + song.tracks[0].name,
+                    "Release Date: "+ song.tracks[0].album.release_date,
                 ].join ("\n\n");
-                console.log(trackData);
-        })
-        
+            // console.log(trackData);
+            })
+            .catch(function(err) {
+                console.error('Error occurred: ' + err); 
+            });
+        }) 
         .catch(function(err) {
-          console.error('Error occurred: ' + err); 
-        });
-        })
-        
-        .catch(function(err) {
-          console.error('Error occurred: ' + err); 
+            console.error('Error occurred: ' + err); 
         });
         break;
-        case 'concert-this':
-        bandsInTown
-        .request('https://api.spotify.com/v1/search?q=' + bandName +'&type=artist')
-        .then(function(artist) {
-          console.log('asdfasdfasfasdfasdfada');   
-        })
-        .catch(function(err) {
-          console.error('Error occurred: ' + err); 
-        });
+        
+        
+    case 'concert-this':
+
+        axios.get('https://rest.bandsintown.com/artists/' + argString + '/events?app_id=codingbootcamp')
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
         break;
-    default : 
-    console.log("You Fucked up")
-    }
+
+default : 
+console.log("You Fucked up")
+}
 
 
 
